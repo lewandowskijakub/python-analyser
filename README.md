900ee823c31305a454852432ece754fa507fba39
marwin1991 <peter.zmilczak@gmail.com>
2020-10-17 20:01:33 +0200

    main project requirements

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # python-analyzes
+(fork this project to your github account, if you want imporve the tasks, the readme or add some unit test feel free to create pull request)
+
+This project was created to present the difficulty of interpreting and analyzing
+code performed by the interepretes. 
+
+As a developer, your goal is to implement all the functions in python_analyser.py
+
+For those that are interested in the origin of to_analysed.py file:
+
+File "to_analysed.py" is a file that is used to
+test implemented code - it is a simple program that perform some truth validation, to run it first run in console:
+
+```
+pip install requests
+pip install build
+pip install google-api-python-client
+```
+
+Then you can run it with: python to_analyse.py 
+And give some statment that can be validated for example: Peter is good mentor
+For the result you have to wait a bit but it should be something like this:
+
+```
+Report for statement: Peter is good mentor
+------------------------------------------
+The truth ratio for: Peter is good mentor is: 0.5
+This means that this statement is: not false and not true, cannot say.
+
+```
+
+
---
+++ python_analyser.py
@@ -0,0 +1,37 @@
+
+def count_lines(filename):
+    # this function returns number of all file lines
+    pass
+
+def count_not_empty_lines(filename):
+    # this function returns number of non-empty lines
+    pass
+
+
+def count_comment_lines(filename):
+    # this function returns number of comment lines
+    # dont forget that comment starts with hash "#" 
+    pass
+
+def count_code_lines(filename):
+    # this function returns number of code lines
+    pass
+
+def count_words(filename):
+    # this function returns number of words in file (#,:,etc, ==, +, etc) are not words
+    pass
+
+def count_word(filename, word):
+    # this function returns number of occurence of word in the file
+    pass
+
+def count_comment_words(filename):
+    # this function returns number of words in all comments
+    pass
+
+
+def count_syntax(filename, syntax_word):
+    # this function should handle following syntax words: if, else, elif, for, while, return, class, def, try, except
+    # remember that "if" can occure in comments and commented lines of code f.e #while True: 
+    # should not be counted. This function should return the number of syntax_word occurence in the file.
+    pass
